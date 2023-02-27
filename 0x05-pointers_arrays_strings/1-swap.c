ef880a149744aa02952697a25ae08a6216cc4a97
Chidex22 <chidex7119@gmail.com>
2023-02-27 15:44:10 -0800

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,15 +2,14 @@
 
 /**
  * swap_int - Swaps the values of two integers.
- * @a: The first integer to be swapped.
- * @b: The second integer to be swapped.
- *
- * Retrun: nothing
+ * @a: int a
+ * @b: int b
  */
-
-void swap_int(int *a, int *b);
+void swap_int(int *a, int *b)
 {
-	int temp = *a;
+	int a;
+
+	a = *a;
 	*a = *b;
-	*b = temp;
+	*b = a;
 }
