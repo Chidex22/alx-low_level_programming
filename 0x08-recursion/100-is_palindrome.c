134f96660b9f45cd66ebc803f73254cee3fab3c9
Chidex22 <chidex7119@gmail.com>
2023-03-08 11:51:50 -0800

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,44 +1,57 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns the length of a string.
- * @s: string
- * Return: the length of a string.
+ * get_length - Entry point
+ * Description: Get the legth
+ * @s: Character
+ * Return: 1 if palindrome 0 otherwise
  */
-int _strlen_recursion(char *s)
+
+int get_length(char *s)
 {
 	if (*s == '\0')
+	{
 		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
+	}
+	return (1 + get_length(s + 1));
 }
 
 /**
- * comparator - compares each character of the string.
- * @s: string
- * @n1: smallest iterator.
- * @n2: biggest iterator.
- * Return:
+ * check_palindrome - Entry point
+ * Description: Checking Palindrome
+ * @s: Character
+ * @start: Integer
+ * @end: Integer
+ * Return: 1 if palindrome 0 otherwise
  */
-int comparator(char *s, int n1, int n2)
+
+int check_palindrome(char *s, int start, int end)
 {
-	if (*(s + n1) == *(s + n2))
+	if (start >= end)
 	{
-		if (n1 == n2 || n1 == n2 + 1)
-			return (1);
-		return (0 + comparator(s, n1 + 1, n2 - 1));
+		return (1);
+	}
+	if (*(s + start) == *(s + end))
+	{
+		return (check_palindrome(s, start + 1, end - 1));
 	}
 	return (0);
 }
 
 /**
- * is_palindrome - detects if a string is a palindrome.
- * @s: string.
- * Return: 1 if s is a palindrome, 0 if not.
+ * is_palindrome - Entry point
+ * Description: Palindrome
+ * @s: Character
+ * Return: 1 if palindrome 0 otherwise
  */
+
 int is_palindrome(char *s)
 {
-	if (*s == '\0')
+	int len = get_length(s);
+
+	if (len == 0 || len == 1)
+	{
 		return (1);
-	return (comparator(s, 0, _strlen_recursion(s) - 1));
+	}
+	return (check_palindrome(s, 0, len - 1));
 }
