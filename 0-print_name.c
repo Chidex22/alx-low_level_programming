ab7c7c7436a567c13ec47b235aafced4e954aa37
Chidex22 <chidex7119@gmail.com>
2023-03-22 14:30:18 -0700

    0-print_name.c

--- 0-print_name.c
+++ 0-print_name.c
@@ -1,20 +1,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Entry point
- * Description: Prints a name.
- * @name: The name to print.
- * @f: A function that takes a character pointer argument and returns void.
- * Return: void
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
-	{
-		return;
-	}
-	f(name);
-	/* Call the function pointer f with the argument name */
-
+	if (name && f)
+		f(name);
 }
