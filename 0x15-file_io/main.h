9d2d444a9a0d6f63148dee0f5dd74ee2ee7a2c82
Chidex22 <chidex7119@gmail.com>
2023-04-10 09:28:21 -0700

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,26 +1,15 @@
-#ifndef MY_MAIN_H
-#define MY_MAIN_H
+#ifndef _MAIN_
+#define _MAIN_
 
-/* Define buffer size for task 3 */
-#define BUFFER_SIZE 1024
-
-/**
- * Description: Header file containing prototypes for all functions
- * for the 0x15-file_io project
- */
-
-/* Libraries Used in the Project */
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-/* Prototypes */
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
 
-#endif /* MY_MAIN_H */
+#endif
